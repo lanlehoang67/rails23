891c3cdc8c65d14e528d7b7724cebdf9708c336f
Lan <lanlehoang10@gmail.com>
2018-08-11 22:08:33 +0700

    Heroku config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Portfolio
   class Application < Rails::Application
+# config/application.rb
+config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
